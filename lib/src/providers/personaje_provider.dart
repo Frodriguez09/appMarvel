d109e8ff949950c3e437eb6696edefac7a6d610d
Fernando Rodriguez <frodriguezix@gmail.com>
2021-07-05 23:48:16 -0800

    correcion de provider

--- lib/src/providers/personaje_provider.dart
+++ lib/src/providers/personaje_provider.dart
@@ -8,7 +8,7 @@ class PersonajeProvider {
 
   Future<List<CharacterModel>> getCharacters() async {
     final response = await _http.get(_url);
-    List<dynamic> data = response.data;
+    List<dynamic> data = response.data['results'];
     return data
         .map((character) => CharacterModel.fromJsonCharacter(character))
         .toList();
